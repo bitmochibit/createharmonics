24673801669fc35c75d3a5bf4e78c33f79027497
Corvinoo <39708435+Corvinoo@users.noreply.github.com>
2025-10-22 19:23:51 +0200

    Fix ItemProperty instantiation in registry

--- src/main/kotlin/me/mochibit/createharmonics/registry/ModItemsRegistry.kt
+++ src/main/kotlin/me/mochibit/createharmonics/registry/ModItemsRegistry.kt
@@ -20,8 +20,7 @@ object ModItemsRegistry : AbstractModRegistry {
 
     fun registerEtherealDiscVariant(suffix: Config.DiscType, maxUses: Int?): ItemEntry<EtherealDiscItem> {
         val name = "ethereal_disc_${suffix.name.lowercase()}"
-        
-        return cRegistrate().item(name) { EtherealDiscItem(maxUses == null, props) }
+        return cRegistrate().item(name) { EtherealDiscItem(maxUses == null, Item.Properties()) }
             .properties { prop: Item.Properties -> prop.stacksTo(1) }
             .model { ctx, prov ->
                 prov.generated(ctx, prov.modLoc("item/$name"))
