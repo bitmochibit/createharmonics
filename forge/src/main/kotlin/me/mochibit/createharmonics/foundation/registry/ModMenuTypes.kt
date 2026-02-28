8d2e4dd6ecb5d564d4441343496a909dc6ae81bc
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-02-28 18:52:35 +0100

    Refactorssss
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

--- forge/src/main/kotlin/me/mochibit/createharmonics/foundation/registry/ModMenuTypes.kt
+++ forge/src/main/kotlin/me/mochibit/createharmonics/foundation/registry/ModMenuTypes.kt
@@ -1,13 +1,14 @@
 package me.mochibit.createharmonics.foundation.registry
 
-import dev.architectury.registry.registries.DeferredRegister
 import me.mochibit.createharmonics.CreateHarmonicsMod
+import me.mochibit.createharmonics.ModEventBus
 import net.minecraft.core.registries.Registries
+import net.minecraftforge.registries.DeferredRegister
 
 object ModMenuTypes : Registrable {
-    private val MENUS = DeferredRegister.create(CreateHarmonicsMod.MOD_ID, Registries.MENU)
+    private val MENUS = DeferredRegister.create(Registries.MENU, CreateHarmonicsMod.MOD_ID)
 
     override fun register() {
-        MENUS.register()
+        MENUS.register(ModEventBus)
     }
 }
--- forge/src/main/kotlin/me/mochibit/createharmonics/foundation/registry/ModSounds.kt
+++ forge/src/main/kotlin/me/mochibit/createharmonics/foundation/registry/ModSounds.kt
@@ -1,20 +1,22 @@
 package me.mochibit.createharmonics.foundation.registry
 
-import dev.architectury.registry.registries.DeferredRegister
-import dev.architectury.registry.registries.RegistrySupplier
+import com.tterrag.registrate.util.entry.RegistryEntry
 import me.mochibit.createharmonics.CreateHarmonicsMod
+import me.mochibit.createharmonics.ModEventBus
 import me.mochibit.createharmonics.foundation.extension.asResource
 import net.minecraft.core.registries.Registries
 import net.minecraft.sounds.SoundEvent
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.RegistryObject
 
 object ModSounds : Registrable {
     private val SOUND_EVENTS: DeferredRegister<SoundEvent> =
-        DeferredRegister.create(CreateHarmonicsMod.MOD_ID, Registries.SOUND_EVENT)
+        DeferredRegister.create(Registries.SOUND_EVENT, CreateHarmonicsMod.MOD_ID)
 
     val SLIDING_STONE = registerSoundEvent("sliding_stone")
     val GLITTER = registerSoundEvent("glitter")
 
-    private fun registerSoundEvent(name: String): RegistrySupplier<SoundEvent> =
+    private fun registerSoundEvent(name: String): RegistryObject<SoundEvent> =
         SOUND_EVENTS.register(name) {
             SoundEvent.createVariableRangeEvent(
                 name.asResource(),
@@ -22,6 +24,6 @@ object ModSounds : Registrable {
         }
 
     override fun register() {
-        SOUND_EVENTS.register()
+        SOUND_EVENTS.register(ModEventBus)
     }
 }
