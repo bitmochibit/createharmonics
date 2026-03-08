937657ee6e25827491989f62fb10476d211b6749
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-03-08 21:39:19 +0100

    Merge with loot tables features
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

--- forge/src/main/kotlin/me/mochibit/createharmonics/foundation/registry/ModLootModifiers.kt
+++ forge/src/main/kotlin/me/mochibit/createharmonics/foundation/registry/ModLootModifiers.kt
@@ -1,33 +1,25 @@
-package me.mochibit.createharmonics.registry
+package me.mochibit.createharmonics.foundation.registry
 
 import com.mojang.serialization.Codec
 import me.mochibit.createharmonics.CreateHarmonicsMod
+import me.mochibit.createharmonics.ModEventBus
 import me.mochibit.createharmonics.data.lootModifier.AddItemModifier
 import net.minecraftforge.common.loot.IGlobalLootModifier
-import net.minecraftforge.eventbus.api.IEventBus
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import net.minecraftforge.registries.DeferredRegister
 import net.minecraftforge.registries.ForgeRegistries
 import net.minecraftforge.registries.RegistryObject
 
-object ModLootModifiers: AutoRegistrable {
-
+object ModLootModifiers : ForgeRegistry {
     val LOOT_MODIFIERS: DeferredRegister<Codec<out IGlobalLootModifier>> =
         DeferredRegister.create(
             ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS,
-            CreateHarmonicsMod.MOD_ID
+            CreateHarmonicsMod.MOD_ID,
         )
 
     val ADD_ITEM: RegistryObject<Codec<AddItemModifier>> =
         LOOT_MODIFIERS.register("add_record_to_end_ship") { AddItemModifier.CODEC }
-    
 
-    override fun register(
-        eventBus: IEventBus,
-        context: FMLJavaModLoadingContext
-    ) {
-        LOOT_MODIFIERS.register(eventBus)
-        println("Registering loot modifiers!")
-
+    override fun register() {
+        LOOT_MODIFIERS.register(ModEventBus)
     }
 }
