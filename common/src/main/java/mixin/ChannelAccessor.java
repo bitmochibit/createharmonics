3516d18b0dc60d415529fad58831b6c805304554
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-04-17 16:12:40 +0200

    Optimized imports
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

--- common/src/main/java/mixin/ChannelAccessor.java
+++ common/src/main/java/mixin/ChannelAccessor.java
@@ -2,7 +2,6 @@ package mixin;
 
 import com.mojang.blaze3d.audio.Channel;
 import net.minecraft.client.sounds.AudioStream;
-import net.minecraft.client.sounds.ChannelAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
--- common/src/main/java/mixin/ChannelMixin.java
+++ common/src/main/java/mixin/ChannelMixin.java
@@ -1,20 +1,13 @@
 package mixin;
 
 import com.mojang.blaze3d.audio.Channel;
-import com.simibubi.create.content.kinetics.mechanicalArm.ArmBlockEntity;
 import me.mochibit.createharmonics.audio.stream.PausableAudioStream;
 import net.minecraft.client.sounds.AudioStream;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.Property;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Optional;
 
 @Mixin(Channel.class)
 public abstract class ChannelMixin {
